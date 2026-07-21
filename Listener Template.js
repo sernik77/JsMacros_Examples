0610f5866030d0777a32c27dac4710c7579c3788
Sernik <serainox@gmail.com>
2026-07-21 22:12:16 +0200

    Remove root duplicate (already in macros/)

--- Listener Template.js
+++
@@ -1,9 +0,0 @@
-var N1 = '<Nickname> '    // Nickname to listen for
-var MSG1 = 'Message'          // Trigger message
-
-if (event.text.getString().includes(N1 + MSG1))
-Chat.say("ACTION DETECTED") // Reply 
-
-//JsMacros.runScript("script.js")
-// Use runScript instead of Chat.say to execute longer tasks
- 
